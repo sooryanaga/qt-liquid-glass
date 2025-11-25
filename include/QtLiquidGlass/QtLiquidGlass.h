9e7f1c2c764fbd4c8a45c6e9c9a7080b962ce861
fsalinas26 <salinasf3@gmail.com>
2025-11-25 07:45:00 -0800

    Initial commit: Project structure and CMake scaffolding

---
+++ include/QtLiquidGlass/QtLiquidGlass.h
@@ -0,0 +1,13 @@
+#ifndef QTLIQUIDGLASS_H
+#define QTLIQUIDGLASS_H
+
+#include <QWidget>
+#include <QString>
+
+namespace QtLiquidGlass {
+
+// Placeholder API
+}
+
+#endif // QTLIQUIDGLASS_H
+
---
+++ src/QtLiquidGlassCommon.h
@@ -0,0 +1,8 @@
+#ifndef QTLIQUIDGLASS_COMMON_H
+#define QTLIQUIDGLASS_COMMON_H
+
+#if defined(__APPLE__) && defined(__MACH__)
+    #define PLATFORM_OSX
+#endif
+
+#endif // QTLIQUIDGLASS_COMMON_H
